8a415eeaf413b7991c912ef4a79657a3fed0116f
duythnh <duytle02@gmail.com>
2024-10-07 00:40:34 +0700

    add script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const number_keys = document.querySelectorAll("[data-number]");
+const operator_keys = document.querySelectorAll("[data-operator]");
+const equal_key = document.querySelector("[data-equal]");
+const all_clear_key = document.querySelector("[data-all-clear]");
+const delete_key = document.querySelector("[data-delete]");
+const display_input = document.querySelector("[data-input]");
+const display_output = document.querySelector("[data-output]");
